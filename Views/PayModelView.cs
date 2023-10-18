d294922a8fed859abdef65639684ba43b000be84
Sebastián Carrera Giraldo <sebastian.carrerag123@gmail.com>
2023-10-18 16:39:17 -0500

    Pay Mode Presenter

--- Views/PayModelView.cs
+++ Views/PayModelView.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Data;
 using System.Drawing;
 using System.Linq;
@@ -10,12 +11,83 @@ using System.Windows.Forms;
 
 namespace Supermarket__mvp.Views
 {
-    public partial class PayModelView : Form
+    public partial class PayModelView : Form, IPayModeView
     {
+        public string PayModeId
+        {
+            get { return txtPayModeId.Text; }
+
+            set { txtPayModeId.Text = value; }
+        }
+
+        public string PayModeName
+        {
+            get { return TxtPayModeName.Text; }
+
+            set { TxtPayModeName.Text = value; }
+        }
+
+        public string PayModeObservation
+        {
+            get { return TxtPayModeObservation.Text; }
+
+            set { TxtPayModeObservation.Text = value; }
+        }
+
+        public string SearchValue
+        {
+            get { return txtSearch.Text; }
+
+            set { txtSearch.Text = value; }
+        }
+
+        public bool IsEdit
+        {
+            get { return IsEdit; }
+
+            set { IsEdit = value; }
+        }
+
+        public bool IsSuccessful
+        {
+            get { return IsSuccessful; }
+
+            set { IsSuccessful = value; }
+        }
+
+        public string Message
+        {
+            get { return Message; }
+            set { Message = value; }
+        }
+
         public PayModelView()
         {
             InitializeComponent();
+            AssociateAndRaiseViewEvents();
+
+            tabControl1.TabPages.Remove(tabPagePayModelDetail);
         }
+
+        private void AssociateAndRaiseViewEvents()
+        {
+            BtnSearch.Click += delegate { SearchEvent?.Invoke(this, EventArgs.Empty); };
+
+            txtSearch.KeyDown += (s, e) =>
+            {
+                if (e.KeyCode == Keys.Enter)
+                {
+                    SearchEvent?.Invoke(this, EventArgs.Empty);
+                }
+            };
+        }
+
+        public event EventHandler SearchEvent;
+        public event EventHandler AddNewEvent;
+        public event EventHandler EditEvent;
+        public event EventHandler DeleteEvent;
+        public event EventHandler SaveEvent;
+        public event EventHandler CancelEvent;
 
         private void label1_Click(object sender, EventArgs e)
         {
@@ -25,6 +97,11 @@ namespace Supermarket__mvp.Views
         private void btnDelete_Click(object sender, EventArgs e)
         {
 
+        }
+
+        public void SetPayModeListBildingSource(BindingSource payModeList)
+        {
+            DgPayMode.DataSource = payModeList;
         }
     }
 }
