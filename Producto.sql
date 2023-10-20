517547be71f16a89174e92acdc7f4e37cfd91c1e
Sebastián Carrera Giraldo <sebastian.carrerag123@gmail.com>
2023-10-20 15:11:00 -0500

    Initial Project Producto

---
+++ Producto.sql
@@ -0,0 +1,8 @@
+﻿use Supermarker
+create table Producto
+(
+ Product_Id int identity (100000,1) primary key,
+ Product_Name nvarchar (50) not null,
+ Product_Observation nvarchar (50) not null,
+)
+
