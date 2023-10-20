2f842c623229e0d6229587ca113670a93b8e61be
Sebastián Carrera Giraldo <sebastian.carrerag123@gmail.com>
2023-10-20 09:58:28 -0500

    Pay Mode Repository Categorie _Search

--- Presenters/PayModePresenterCategorie.cs
+++ Presenters/PayModePresenterCategorie.cs
@@ -3,10 +3,83 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Supermarket__mvp.Views;
+using Supermarket__mvp.Models;
 
 namespace Supermarket__mvp.Presenters
 {
     internal class PayModePresenterCategorie
     {
+        private IPayModeViewCategorie view;
+        private IPayModeRepositoryCategorie repository;
+        private BindingSource payModeBindingSource;
+        private IEnumerable<PayModeModelCategorie> payModeList;
+
+        public PayModePresenterCategorie(IPayModeViewCategorie view, IPayModeRepositoryCategorie repository)
+        {
+            this.payModeBindingSource = new BindingSource();
+
+            this.view = view;
+            this.repository = repository;
+
+            this.view.SearchEvent += SearchPayMode;
+            this.view.AddNewEvent += AddNewPayMode;
+            this.view.EditEvent += LoadSelectPayModeToEdit;
+            this.view.DeleteEvent += DeleteSelectedPayMode;
+            this.view.SaveEvent += SavePayMode;
+            this.view.CancelEvent += CancelAction;
+
+            this.view.SetPayModeListBildingSource(payModeBindingSource);
+
+            loadAllPayModeList();
+
+            this.view.Show();
+        }
+
+        private void loadAllPayModeList()
+        {
+            payModeList = repository.GetAll();
+            payModeBindingSource.DataSource = payModeList;
+        }
+
+        private void CancelAction(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SavePayMode(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DeleteSelectedPayMode(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void LoadSelectPayModeToEdit(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddNewPayMode(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SearchPayMode(object? sender, EventArgs e)
+        {
+            bool emptyValue = string.IsNullOrWhiteSpace(this.view.SearchValue);
+            if (emptyValue == false)
+            {
+                payModeList = repository.GetByValue(this.view.SearchValue);
+            }
+            else
+            {
+                payModeList = repository.GetAll();
+            }
+            payModeBindingSource.DataSource = payModeList;
+        }
     }
+
 }
---
+++ _Repositories/PayModeRepositoryCategorie.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Microsoft.Data;
+using Supermarket__mvp.Models;
+using System.Data;
+
+namespace Supermarket__mvp._Repositories
+{
+    internal class PayModeRepositoryCategorie : BaseRepository, IPayModeRepositoryCategorie
+    {
+        public PayModeRepositoryCategorie(string ConnectionString) 
+        {
+            this.connectionString = ConnectionString;
+        }
+        public void Add(PayModeModelCategorie payModeModelCategorie)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(PayModeModelCategorie payModeModelCategorie)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<PayModeModelCategorie> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<PayModeModelCategorie> GetByValue(string value)
+        {
+            var payModeList = new List <PayModeModelCategorie>();
+            using (var connection = new SqlConnection("Data Source=DESKTOP-012JM51;Initial Catalog=Supermarker;Integrated Security=True;TrustServerCertificate=True;"))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "SELECT * FROM PayMode ORDER BY Pay_Mode_ID DESC";
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var PayModeModelCategorie = new PayModeModelCategorie();
+                        PayModeModelCategorie.Id = (int)reader["Pay_Mode_Categorie_Id"];
+                        PayModeModelCategorie.Name = reader["Pay_Mode_Categorie_Name"].ToString();
+                        PayModeModelCategorie.Observation = reader["Pay_Mode_Categorie_Observation"].ToString();
+                        payModeList.Add(PayModeModelCategorie);
+                    }
+                }
+            }
+            return payModeList;
+        }
+    }
+}
