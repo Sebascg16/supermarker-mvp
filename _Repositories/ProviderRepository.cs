42728e4da3d552951c74081c21507d737428cc43
Sebastián Carrera Giraldo <sebastian.carrerag123@gmail.com>
2023-10-20 23:44:14 -0500

    Provider repository - Search

---
+++ _Repositories/ProviderRepository.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Microsoft.Data;
+using Supermarket__mvp.Models;
+using System.Data;
+
+namespace Supermarket__mvp._Repositories
+{
+    internal class ProviderRepository : BaseRepository, IProviderRepository
+    {
+        public ProviderRepository(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public void Add(ProviderModel providerModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(ProviderModel providerModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ProviderModel> GetAll()
+        {
+            var providerList = new List<ProviderModel>();
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "SELECT * FROM Provider ORDER BY Provider_Id DESC";
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var providerModel = new ProviderModel();
+                        providerModel.Id = (int)reader["Provider_Id"];
+                        providerModel.Name = reader["Provider_Name"].ToString();
+                        providerModel.Observation = reader["Provider_Observation"].ToString();
+                        providerList.Add(providerModel);
+                    }
+                }
+            }
+            return providerList;
+        }
+
+        public IEnumerable<ProviderModel> GetByValue(string value)
+        {
+            var providerList = new List<ProviderModel>();
+            int providerId = int.TryParse(value, out _) ? Convert.ToInt32(value) : 0;
+            string providerName = value;
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = @"SELECT * FROM Provider
+                                WHERE Provider_Id=@id or Provider_Name LIKE @name+ '%'
+                                ORDER By Provider_Id DESC";
+                command.Parameters.Add("@id", SqlDbType.Int).Value = providerId;
+                command.Parameters.Add("@name", SqlDbType.NVarChar).Value = providerName;
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var providerModel = new ProviderModel();
+                        providerModel.Id = (int)reader["Provider_Id"];
+                        providerModel.Name = reader["Provider_Name"].ToString();
+                        providerModel.Observation = reader["Provider_Observation"].ToString();
+                        providerList.Add(providerModel);
+                    }
+                }
+
+            }
+            return providerList;
+        }
+    }
+}
